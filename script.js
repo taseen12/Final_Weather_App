cb051dd0f8e58f9f9efd6b517fa0c02b5c529539
taseen12 <elantaseen@gmail.com>
2023-12-13 12:52:04 +0600

    last commit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function scrollToEnd() {
-    // Scroll to the bottom of the page
+    
     window.scrollTo({
         top: document.body.scrollHeight,
         behavior: 'smooth'
     });
 }
 function searchWeather() {
-    const apiKey = '6c64698dc5c9adcc83b19e5baf735ed3'; // Replace with your OpenWeatherMap API key
+    const apiKey = '6c64698dc5c9adcc83b19e5baf735ed3'; 
     const city = document.getElementById('cityInput').value;
 
     if (!city) {
@@ -38,14 +38,17 @@ function displayWeather(data) {
     const windSpeed = data.wind.speed;
     const country = data.sys.country;
 
-    let weatherImage;
+    
 
     // Choose an image based on temperature and weather condition
-    if (temperature > 30) {
-        weatherImage = 'Image/cold.jfif';
-    } else if (temperature > 20) {
-        weatherImage = 'Image/cold.jfif';
-    } else {
+    let weatherImage;
+    if (temperature > 295) {
+        weatherImage = 'Image/sunny.png';
+    } else if (temperature > 293) {
+        weatherImage = 'Image/rainy.png';
+        
+    }
+    else if (temperature > 20) {
         weatherImage = 'Image/cold.jfif';
     }
 
@@ -62,7 +65,7 @@ document.getElementById('weatherData').appendChild(imgElement);
             <h2>Current Weather Data</h2>
             <div class="weather-details">
                 <h3>${cityName}, ${country}</h3>
-                <p>Temperature: ${temperature} &deg;C</p>
+                <p>Temperature: ${temperature} &deg;F</p>
                 <p>Weather: ${weatherDescription}</p>
                 <p>Humidity: ${humidity}%</p>
                 <p>Wind Speed: ${windSpeed} m/s</p>
@@ -72,4 +75,78 @@ document.getElementById('weatherData').appendChild(imgElement);
     `;
 
     weatherDataContainer.innerHTML = weatherHTML;
+}
+
+function searchWeather2() {
+    const apiKey = '6c64698dc5c9adcc83b19e5baf735ed3'; 
+    const city = document.getElementById('cityInput2').value;
+
+    if (!city) {
+        alert('Please enter a city.');
+        return;
+    }
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayForecastDetails(data);
+        })
+        .catch(error => {
+            console.error('Error fetching forecast data:', error);
+            alert('Error fetching forecast data. Please try again.');
+        });
+}
+
+function displayForecastDetails(data) {
+    console.log(data); // Output the entire data to the console for inspection
+
+    const forecastDataContainer = document.getElementById('weatherData2');
+    forecastDataContainer.innerHTML = '';
+
+    const cityName = data.city.name;
+    const country = data.city.country;
+
+    const forecastList = data.list;
+
+    forecastList.forEach(item => {
+        const forecastTime = new Date(item.dt * 1000);
+        const temperature = item.main.temp;
+        const weatherDescription = item.weather[0].description;
+        const humidity = item.main.humidity;
+        const windSpeed = item.wind.speed;
+        const cloudiness = item.clouds.all;
+
+        // Choose an image based on temperature
+        let weatherImage;
+        if (temperature > 295) {
+            weatherImage = 'Image/sunny.png';
+        } else if (temperature > 293) {
+            weatherImage = 'Image/rainy.png';
+            
+        }
+        else if (temperature > 20) {
+            weatherImage = 'Image/cold.jfif';
+        }
+
+        const forecastHTML = `
+            <div class="forecast-card">
+                <img src="${weatherImage}" alt="Weather Image" class="weather-image">
+                <h3>${formatTime(forecastTime)}</h3>
+                <p>Temperature: ${temperature} &deg;F</p>
+                <p>Weather: ${weatherDescription}</p>
+                <p>Humidity: ${humidity}%</p>
+                <p>Wind Speed: ${windSpeed} m/s</p>
+                <p>Cloudiness: ${cloudiness}%</p>
+            </div>
+        `;
+
+        forecastDataContainer.innerHTML += forecastHTML;
+    });
+}
+
+function formatTime(date) {
+    const options = { hour: 'numeric', minute: 'numeric', hour12: true, timeZone: 'UTC' };
+    return date.toLocaleTimeString('en-US', options);
 }
